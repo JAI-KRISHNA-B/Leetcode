9a9f2aa51c6da2eeba720bc75ef21e1f6dd1abef
Jaikrishna B <bjaikrishna33@gmail.com>
2025-06-12 22:40:53 +0530

    Time: 1 ms (99.84%), Space: 53.1 MB (49.4%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int elem = 0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0)
+            elem = nums[i];
+
+            if(elem==nums[i])
+            count+=1;
+            else
+            count-=1;
+        }
+        return elem;
+    }
+}
