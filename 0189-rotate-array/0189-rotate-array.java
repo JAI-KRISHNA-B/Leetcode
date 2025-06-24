00b3930a8f87efe4f0dad12a3297b19821345a25
Jaikrishna B <bjaikrishna33@gmail.com>
2025-06-24 18:16:02 +0530

    Time: 0 ms (100%), Space: 56.9 MB (94.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -5,14 +5,14 @@ class Solution {
 
         k = k%n;
 
-        reverse(arr,0,n-k-1);
+        reverse(arr,(n-k),(n-1));
 
-        reverse(arr,n-k,n-1);
+        reverse(arr,0,(n-k-1));
 
-        reverse(arr,0,n-1);
+        reverse(arr,0,(n-1));
     }
 
-    static void reverse(int[] arr,int x,int y){
+    void reverse(int[] arr,int x,int y){
         while(x<y){
             int temp = arr[x];
             arr[x] = arr[y];
